905c0da7965888baa7f2de2a1b63134c52efcad2
miriamjorna <hoimirextra@gmail.com>
2026-06-11 21:10:05 +0100

    repairs on artist info and test

--- Project-Music-Data/common.test.mjs
+++ Project-Music-Data/common.test.mjs
@@ -1,7 +1,7 @@
 import assert from "node:assert";
 import test from "node:test";
 import { countUsers } from "./common.mjs";
-import { getListenEvents } from "./data.mjs";
+import { getSong, getListenEvents } from "./data.mjs";
 import {
   getMostListenedSong,
   getLongestStreak,
@@ -13,11 +13,11 @@ test("User count is correct", () => {
 
 test("Most listened song for user 1 is When Your Mind's Made Up", () => {
   const events = getListenEvents("1");
-  const song = getMostListenedSong(events);
+  const song = getMostListenedSong(events, getSong);
   assert.equal(song.title, "When Your Mind's Made Up");
 });
 
 test("Longest streak for user 2 is 44", () => {
   const events = getListenEvents("2");
-  assert.equal(getLongestStreak(events).count, 44);
+  assert.equal(getLongestStreak(events, getSong).count, 44);
 });
