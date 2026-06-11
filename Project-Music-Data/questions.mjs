6c445e79ddb8bc48e02a57f41f6ad0c86b6bc4f8
miriamjorna <hoimirextra@gmail.com>
2026-06-11 18:44:21 +0100

    more questions  added

--- Project-Music-Data/questions.mjs
+++ Project-Music-Data/questions.mjs
@@ -12,3 +12,32 @@ export function getMostListenedSong(
   )[0];
   return getSongFn(topId);
 }
+
+export function getMostListenedArtist(
+  listenEvents,
+  getArtistFn,
+) {
+  const counts = {};
+  for (const event of listenEvents) {
+    counts[event.artist_id] =
+      (counts[event.artist_id] || 0) + 1;
+  }
+  const topId = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a],
+  )[0];
+  return getArtistFn(topId);
+}
+
+export function getMostListenedSong(
+  listenEvents,
+  getSongFn,
+) {
+  const sum = {};
+  for (const event of listenEvents) {
+    sum[event.song_id] = (sum[event.song_id] || 0) + 1;
+  }
+  const topId = Object.keys(sum).sort(
+    (a, b) => sum[b] - sum[a],
+  )[0];
+  return getSongFn(topId);
+}
